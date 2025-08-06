4d0c8701b93eeec03c5fa3a4276e9b264773a302
kimura79 <ema.saracino@gmail.com>
2025-08-06 16:13:20 +0200

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'camera_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Epidermys AI',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: const CameraPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
